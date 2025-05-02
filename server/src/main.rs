558dff41f7a9931674d7935beb79f2a74e6b9e0b
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-05-02 14:46:09 -0400

    axum hello world

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 8080
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("Listening on http://localhost:8080");
+    axum::serve(listener, app).await.unwrap();
 }
